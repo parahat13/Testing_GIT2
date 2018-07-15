abe1968eb3c647711b04785e10028d5a7fcb0985
Parahat <parahat1380@gmail.com>
2018-07-15 15:21:10 -0500

    fixed

--- src/objectRepository/LoginPage.java
+++ src/objectRepository/LoginPage.java
@@ -3,12 +3,8 @@ package objectRepository;
 public class LoginPage {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		//
-
 		
-		System.out.println("First  tester  added  some  code ");
-		System.out.println("First  tester  added  second  code ");
+		System.out.println("Conflict has fixed");
 		
 		
 
