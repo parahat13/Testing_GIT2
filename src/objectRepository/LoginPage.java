2f8d2efc10bd6990c9864425a128ba8734f7fb2d
Parahat <parahat1380@gmail.com>
2018-07-15 15:28:21 -0500

    updated

--- src/objectRepository/LoginPage.java
+++ src/objectRepository/LoginPage.java
@@ -4,22 +4,11 @@ public class LoginPage {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		//
-
 		
 		System.out.println("First  tester  added  some  code ");
-
 		System.out.println("Second  tester  added  some  code ");
 		System.out.println("Second  tester  added  some  code ");
-
-		System.out.println("First  tester  added  second  code ");
 		
-		System.out.println("First  tester  added  second  code ");
-		
-		
-
-		
-
 	}
 
 }
