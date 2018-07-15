8f80b69454bafd8c08f1574a9d76a82254330a31
Parahat <parahat1380@gmail.com>
2018-07-15 09:39:53 -0500

    added  new  code

--- src/objectRepository/LoginPage.java
+++ src/objectRepository/LoginPage.java
@@ -10,6 +10,13 @@ public class LoginPage {
 		System.out.println("First  tester  added  some  code ");
 		System.out.println("First  tester  added  second  code ");
 		
+		System.out.println("First  tester  added  some  code ");
+		System.out.println("First  tester  added  second  code ");
+		
+		System.out.println("First  tester  added  some  code ");
+		System.out.println("First  tester  added  second  code ");
+		
+		
 		
 
 	}
